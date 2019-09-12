d65387cc9ec04b4beb1a7d1caa33ab53bbefec2f
Aaron <ahelman@csumb.edu>
2019-09-11 23:18:34 -0700

    please

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/project-test1'));
+app.use(express.static(__dirname + '/dist/project-test'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/project-test1/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/project-test/index.html'));
 });
 
 // default Heroku PORT
