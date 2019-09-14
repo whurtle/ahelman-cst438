c5441b3930c8a899cf684cccb2ac3bf0d875b3ab
whurtle <47097705+whurtle@users.noreply.github.com>
2019-09-13 23:56:15 -0700

    noob style merge

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import {SlideshowModule} from 'ng-simple-slideshow';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    ProductsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    SlideshowModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
